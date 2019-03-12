9c8452f7426070303a8a7623c829fddb9535e29f
Sean Chavez <realseanchavez@gmail.com>
2019-03-12 16:09:10 -0700

    clargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,19 +1,10 @@
-const validator = require('validator');
-const chalk = require('chalk');
-const getNotes = require('./notes.js');
+const chalk = require("chalk");
+const getNotes = require("./notes.js");
 
-const msg = getNotes();
+const command = process.argv[2];
 
-console.log(msg);
-
-console.log(validator.isURL('https://broman.com'));
-
-console.log(chalk.blue.bgRed.bold('Sucess!'));
-
-console.log(
-  chalk.green(
-    'I am a green line ' +
-      chalk.blue.underline.bold('with a blue substring') +
-      ' that becomes green again!',
-  ),
-);
+if (command === "add") {
+  console.log("Adding note!");
+} else if (command === "remove") {
+  console.log("Removing note!");
+}
