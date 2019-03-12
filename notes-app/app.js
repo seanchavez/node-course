7b93d9676019e6141a9e04d6a5f090687b5a1c2f
Sean Chavez <realseanchavez@gmail.com>
2019-03-11 22:51:42 -0700

    chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,11 @@
 const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
 
 console.log(msg);
 
-console.log(validator.isURL('https/broman.com'));
+console.log(validator.isURL('https://broman.com'));
+
+console.log(chalk.blue('Sucess!'));
