f180f2013ddd74e4cc4578f30d7c712872d1dcf6
Sean Chavez <realseanchavez@gmail.com>
2019-03-11 23:37:35 -0700

    nodemon

--- notes-app/app.js
+++ notes-app/app.js
@@ -8,4 +8,12 @@ console.log(msg);
 
 console.log(validator.isURL('https://broman.com'));
 
-console.log(chalk.blue('Sucess!'));
+console.log(chalk.blue.bgRed.bold('Sucess!'));
+
+console.log(
+  chalk.green(
+    'I am a green line ' +
+      chalk.blue.underline.bold('with a blue substring') +
+      ' that becomes green again!',
+  ),
+);
