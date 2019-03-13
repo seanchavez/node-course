6c5c091c4aab19078d795d2b43eaffa447e28876
Sean Chavez <realseanchavez@gmail.com>
2019-03-12 22:49:44 -0700

    jaysun

--- playground/1-json.js
+++ playground/1-json.js
@@ -7,7 +7,15 @@ const fs = require('fs')
 // const bookJSON = JSON.stringify(book)
 // fs.writeFileSync('1-json.json', bookJSON)
 
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
 const dataBuffer = fs.readFileSync('1-json.json')
 const dataJSON = dataBuffer.toString()
 const data = JSON.parse(dataJSON)
-console.log(data.title)
+data.name = 'Sean'
+data.planet = 'unknown'
+data.age = 'n/a'
+fs.writeFileSync('1-json.json', JSON.stringify(data))
