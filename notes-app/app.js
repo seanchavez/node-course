df0f339b3a073fdc98a92b87a8eb9d5fbd5ffc90
Sean Chavez <realseanchavez@gmail.com>
2019-03-12 17:12:19 -0700

    yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,39 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
-const command = process.argv[2];
+yargs.version("1.1.0");
 
-if (command === "add") {
-  console.log("Adding note!");
-} else if (command === "remove") {
-  console.log("Removing note!");
-}
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function() {
+    console.log("Adding a new note!");
+  }
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function() {
+    console.log("Removing a note.");
+  }
+});
+
+yargs.command({
+  command: "list",
+  describe: "List the notes",
+  handler: function() {
+    console.log("Listing the notes!");
+  }
+});
+
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function() {
+    console.log("Reading a note.");
+  }
+});
+
+console.log(yargs.argv);
