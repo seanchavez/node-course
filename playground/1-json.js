223f77984a1016bdb02767c2f72df08a30e9fe42
Sean Chavez <realseanchavez@gmail.com>
2019-04-17 21:42:25 -0700

    late

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,16 +1,4 @@
 const fs = require('fs')
-// const book = {
-//   title: 'Ego is the enemy',
-//   author: 'Ryan Holiday'
-// }
-
-// const bookJSON = JSON.stringify(book)
-// fs.writeFileSync('1-json.json', bookJSON)
-
-// const dataBuffer = fs.readFileSync('1-json.json')
-// const dataJSON = dataBuffer.toString()
-// const data = JSON.parse(dataJSON)
-// console.log(data.title)
 
 const dataBuffer = fs.readFileSync('1-json.json')
 const dataJSON = dataBuffer.toString()
