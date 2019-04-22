b46c8df76c597311a63755d3981b03ba4cdadd5a
Sean Chavez <realseanchavez@gmail.com>
2019-04-22 09:26:10 -0700

    error

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -24,8 +24,6 @@ const addNote = (title, body) => {
   const notes = loadNotes();
   const duplicateNote = notes.find(note => note.title === title);
 
-  debugger;
-
   if (!duplicateNote) {
     notes.push({
       title: title,
